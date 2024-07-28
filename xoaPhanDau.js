caf9a826ec7a009317bd46fd26137c2f8b5d4010
Thành Nam <95743768+az167@users.noreply.github.com>
2024-07-28 19:30:34 +0700

    Add files via upload

---
+++ xoaPhanDau.js
@@ -0,0 +1,7 @@
+/***********************************************
+> deleteHeader by tn2am
+***********************************************/	
+
+const version = 'V1.0.2';
+
+function setHeaderValue(e,a,d){var r=a.toLowerCase();r in e?e[r]=d:e[a]=d}var modifiedHeaders=$request.headers;setHeaderValue(modifiedHeaders,"X-RevenueCat-ETag",""),$done({headers:modifiedHeaders});
